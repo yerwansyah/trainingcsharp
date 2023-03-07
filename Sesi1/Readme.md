74ddfbd5279f536f86d9c379d89f186d35028de6
yerwansyah <yuzzr.rizal@gmail.com>
2023-03-07 09:57:22 +0700

    Sesi 1

---
+++ Sesi1/Readme.md
@@ -0,0 +1,13 @@
+# Sesi 1
+
+## Create Dot Net Project
+- Buat Folder baru hellow
+- jalan kan command
+  `dotnet new console`
+- jalan kan proyek nya
+  `dotnet run`
+
+## Run Project using mono
+- Buat Folder baru hello
+- Tambahkan file baru
+- Gunakan code runner untuk menajalankan projek
---
+++ Sesi1/biodata/biodata.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Biodata
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Nama Lengkap : ");
+        Console.ReadLine();
+        Console.WriteLine("Alamat: ");
+        Console.ReadLine();
+        Console.WriteLine("Hobi: ");
+        Console.ReadLine();
+        Console.WriteLine("Pekerjaan: " );
+        Console.ReadLine();
+    }
+}
